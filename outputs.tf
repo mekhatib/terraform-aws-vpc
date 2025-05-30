0e886ecddfb97ff12590ad1105663472c096ba43
mekhatib <melkhatib@ziggo.nl>
2025-05-30 14:54:11 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,80 +1,70 @@
-# modules/networking/vpc/outputs.tf
-
 output "vpc_id" {
-  description = "The ID of the VPC"
+  description = "ID of the VPC"
   value       = aws_vpc.main.id
 }
 
 output "vpc_cidr_block" {
-  description = "The CIDR block of the VPC"
-  value       = aws_vpc.main.cidr_block
+  description = "CIDR block of the VPC"
+  value       = data.aws_vpc.main.cidr_block
+}
+
+output "vpc_arn" {
+  description = "ARN of the VPC"
+  value       = aws_vpc.main.arn
 }
 
 output "internet_gateway_id" {
-  description = "The ID of the Internet Gateway"
+  description = "ID of the Internet Gateway"
   value       = aws_internet_gateway.main.id
 }
 
-# Subnet outputs
 output "subnet_ids" {
-  description = "List of all subnet IDs"
+  description = "List of subnet IDs"
   value       = aws_subnet.private[*].id
-}
-
-output "private_subnet_ids" {
-  description = "List of private subnet IDs"
-  value = [
-    for idx, subnet in aws_subnet.private : subnet.id
-    if var.subnet_types[idx] == "private"
-  ]
 }
 
 output "public_subnet_ids" {
   description = "List of public subnet IDs"
   value = [
-    for idx, subnet in aws_subnet.private : subnet.id
-    if var.subnet_types[idx] == "public"
+    for i, type in var.subnet_types :
+    aws_subnet.private[i].id if type == "public"
   ]
 }
 
-# For specific subnet types
-output "app_subnet_ids" {
-  description = "List of app subnet IDs (same as private for this module)"
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
   value = [
-    for idx, subnet in aws_subnet.private : subnet.id
-    if var.subnet_types[idx] == "private"
+    for i, type in var.subnet_types :
+    aws_subnet.private[i].id if type == "private"
   ]
 }
 
-output "transit_gateway_subnet_ids" {
-  description = "List of transit gateway subnet IDs (using private subnets)"
-  value = [
-    for idx, subnet in aws_subnet.private : subnet.id
-    if var.subnet_types[idx] == "private"
-  ]
+output "subnet_cidrs" {
+  description = "List of subnet CIDR blocks"
+  value       = aws_subnet.private[*].cidr_block
 }
 
-# Route table outputs
+output "public_route_table_id" {
+  description = "ID of the public route table"
+  value       = length(aws_route_table.public) > 0 ? aws_route_table.public[0].id : null
+}
+
 output "private_route_table_ids" {
   description = "List of private route table IDs"
   value       = aws_route_table.private[*].id
 }
 
-output "public_route_table_ids" {
-  description = "List of public route table IDs"
-  value       = aws_route_table.public[*].id
+output "availability_zones" {
+  description = "List of availability zones used"
+  value       = var.availability_zones
 }
 
-# REMOVE these outputs since the resources don't exist in main.tf:
-# output "flow_log_id" {
-#   description = "Flow Log ID"
-#   value       = try(aws_flow_log.main[0].id, null)
-# }
+output "vpc_main_route_table_id" {
+  description = "ID of the main route table"
+  value       = aws_vpc.main.main_route_table_id
+}
 
-# output "private_zone_id" {
-#   description = "Route53 private zone ID"
-#   value       = try(aws_route53_zone.private[0].zone_id, null)
-# }
-
-# Add this output only if you add Route53 zone creation to main.tf
-# Otherwise, remove any references to private_zone_id in your environment config
+output "vpc_default_security_group_id" {
+  description = "ID of the default security group"
+  value       = aws_vpc.main.default_security_group_id
+}
